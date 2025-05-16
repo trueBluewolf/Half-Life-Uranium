4eec7e6f34e0f551411c10d3be158c38da417e81
Bluewolf <harrisonstidham360@gmail.com>
2025-05-15 17:13:39 -0700

    DecayingUranium changes, Recipe section added, and added item nothing
    DecayingUranium now has new look on belts and ground. Added a new item called nothing, literally is nothing and is only there for decayingUranium to disappear into nothing. Added a section for recipe data to be handled.

--- data.lua
+++ data.lua
@@ -1,3 +1,5 @@
+--#region Items
+
 --#region item Uranium-235
 
 -- Sets up spoil ticks (Ticks = 1/60 of a second)
@@ -6,8 +8,6 @@ data.raw.item["uranium-235"].spoil_ticks = 4 * 60^1 -- default value (4 Hours(4*
 -- Makes Uranium-235 spoil to Uranium-238
 data.raw.item["uranium-235"].spoil_result = "decaying-uranium"
 
--- Always creates fresh Uranium-235 every craft
-data.raw.recipe["kovarex-enrichment-process"].result_is_always_fresh = true
 --#endregion
 
 --#region item Decaying Uranium
@@ -21,8 +21,45 @@ decayingUranium.icons = {
         tint = { r = .5, g = .5, b = .8, a = 1 }
     }
 }
+decayingUranium.pictures = {
+    { size = 64, filename = "__base__/graphics/icons/uranium-ore.png",   scale = 0.5, tint = { r = .5, g = .5, b = .8, a = 1 } },
+    { size = 64, filename = "__base__/graphics/icons/uranium-ore-1.png", scale = 0.5, tint = { r = .5, g = .5, b = .8, a = 1 } },
+    { size = 64, filename = "__base__/graphics/icons/uranium-ore-2.png", scale = 0.5, tint = { r = .5, g = .5, b = .8, a = 1 } },
+    { size = 64, filename = "__base__/graphics/icons/uranium-ore-3.png", scale = 0.5, tint = { r = .5, g = .5, b = .8, a = 1 } }
+}
+decayingUranium.subgroup = "uranium-processing"
+
 decayingUranium.stack_size = 20
 decayingUranium.spoil_ticks = 6 * 60^2 -- default value (6 Mins(6*60^2))
+decayingUranium.spoil_result = "nothing"
 
 data:extend{decayingUranium}
+--#endregion
+
+--#region item Nothing
+local nothing = table.deepcopy(data.raw["item"]["iron-plate"])
+
+nothing.name = "nothing"
+nothing.icons = {
+    {
+        icon = nothing.icon,
+        icon_size = nothing.icon_size,
+        tint = {r = 0, g = 0, b = 0, a = 0}
+    }
+}
+nothing.subgroup = "uranium-processing"
+nothing.order = "zzz"
+
+nothing.spoil_ticks = 1 -- 1 tick (1/60 of a second)
+
+data:extend{nothing}
+--#endregion
+
+--#endregion
+
+--#region Recipes
+
+-- Always creates fresh Uranium-235 every craft
+data.raw.recipe["kovarex-enrichment-process"].result_is_always_fresh = true
+
 --#endregion
